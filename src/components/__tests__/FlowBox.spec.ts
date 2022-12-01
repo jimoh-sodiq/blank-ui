2d153b2b13636fda17c56f64373326b7e4ff85b0
jimoh-sodiq <jimohsodiq301@gmail.com>
2022-12-01 11:39:28 +0100

    completed writing the test for the flowbox component

--- src/components/__tests__/FlowBox.spec.ts
+++ src/components/__tests__/FlowBox.spec.ts
@@ -4,17 +4,38 @@ import { FlowBox } from "../FlowBox";
 
 describe("FlowBox.ts", () => {
   it("renders the component correctly", () => {
-    const wrapper = mount(FlowBox);
+    const wrapper = mount(FlowBox, {});
     expect(wrapper).toBeDefined();
   });
 
-  it("has div as the default tag", () => {});
+  it("has div as the default tag", () => {
+    const wrapper = mount(FlowBox, {});
+    expect(wrapper.find("div")).toBeDefined();
+  });
 
-  it("uses the as props as the tag", () => {});
+  it("uses the as props as the tag", () => {
+    const wrapper = mount(FlowBox, {
+      props: {
+        as: "section",
+      },
+    });
+    expect(wrapper.findAll("div")).toHaveLength(0);
+    expect(wrapper.findAll("section")).toHaveLength(1);
+  });
 
-  it("receives vertical and horizontal props", () => {});
+  it("has a overflow-y-auto if vertical props is true", () => {
+    const wrapper = mount(FlowBox);
+    expect(wrapper.element.style.overflowY).toBe("auto");
+  });
 
-  it("has a overflow-y-auto if vertical props is true", () => {});
-
-  it("has a overflow-x-auto if horizontal props is true", () => {});
+  it("has a overflow-x-auto if horizontal props is true", () => {
+    const wrapper = mount(FlowBox, {
+      props: {
+        horizontal: true,
+        vertical: false,
+      },
+    });
+    expect(wrapper.element.style.overflowX).toBe("auto");
+    expect(wrapper.element.style.overflowY).toBe("hidden");
+  });
 });
